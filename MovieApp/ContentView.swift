7b727b2cb384be95d28df970b0d792e467d80ab5
Matheus Leite de Menezes <matheus.menezes@cabal.com.br>
2020-06-19 12:42:25 -0300

    Initial Commit

---
+++ MovieApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by Matheus Leite on 19/06/20.
+//  Copyright © 2020 Matheus Leite. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
