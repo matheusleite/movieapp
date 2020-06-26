18cf3f5a9bdfcd957131b028eddd15b9c1d5612f
Matheus Leite de Menezes <matheus.menezes@cabal.com.br>
2020-06-26 10:22:27 -0300

    add test for network class

--- MovieAppTests/MovieAppTests.swift
+++ MovieAppTests/MovieAppTests.swift
@@ -10,6 +10,7 @@ import XCTest
 @testable import MovieApp
 
 class MovieAppTests: XCTestCase {
+    var network = MovieNetwork.shared
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,9 +20,54 @@ class MovieAppTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetMoviesRequest() throws {
+        var requestData: Any?
+        var responseError: Error?
+
+        let promise = expectation(description: "Completion handler invoked")
+        
+        network.getMovies(endpoint: "/movie/popular") { (result: Result<MoviesResponse, MovieNetwork.NetworkError>) in
+            switch result {
+                case .success(let movieResponse):
+                    requestData = movieResponse
+                    promise.fulfill()
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    responseError = error
+            }
+        }
+        
+        wait(for: [promise], timeout: 5)
+
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertNotNil(requestData)
+    }
+    
+    func testGetMovieInfoRequest() throws {
+        var requestData: Any?
+        var responseError: Error?
+
+        let promise = expectation(description: "Completion handler invoked")
+        
+        network.getMovieInfo(endpoint: "/movie/475430") { (result: Result<MovieInfo, MovieNetwork.NetworkError>) in
+            switch result {
+                case .success(let movieResponse):
+                    requestData = movieResponse
+                    promise.fulfill()
+                    
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    responseError = error
+            }
+        }
+        
+        wait(for: [promise], timeout: 5)
+
+        // then
+        XCTAssertNil(responseError)
+        XCTAssertNotNil(requestData)
     }
 
     func testPerformanceExample() throws {
